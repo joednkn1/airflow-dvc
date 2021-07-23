98c13882766751d14543c827941d5d68579e9c0d
Piotr Styczyński <piotr@styczynski.in>
2021-07-23 02:32:18 +0200

    Implement existence sensor for DVC

--- airflow_dvc/__init__.py
+++ airflow_dvc/__init__.py
@@ -1,24 +1,25 @@
 from .cli.entrypoint import run_cli
-from .dvc_cli import DVCLocalCli
-from .dvc_download import (DVCCallbackDownload, DVCDownload, DVCPathDownload,
-                           DVCS3Download)
 from .dvc_download_operator import DVCDownloadOperator
 from .dvc_hook import DVCCommit, DVCHook
 from .dvc_update_operator import DVCUpdateOperator
 from .dvc_update_sensor import DVCUpdateSensor
-from .dvc_upload import (DVCCallbackUpload, DVCPathUpload, DVCS3Upload,
-                         DVCStringUpload, DVCUpload)
+from .dvc_existence_sensor import DVCExistenceSensor
+from .custom_uploads import DVCS3Upload
+from .custom_downloads import DVCS3Download
 from .plugin import DVCPlugin
 from . import exceptions
 from . import logs
-from . import config
 from . import stats
 
+from dvc_fs.dvc_download import (DVCCallbackDownload, DVCDownload, DVCPathDownload)
+from dvc_fs.dvc_upload import (DVCCallbackUpload, DVCPathUpload,
+                         DVCStringUpload, DVCUpload)
+
 __all__ = [
-    "DVCLocalCli",
     "DVCHook",
     "DVCUpdateSensor",
     "DVCUpdateOperator",
+    "DVCExistenceSensor",
     "DVCUpload",
     "DVCStringUpload",
     "DVCS3Upload",
@@ -34,7 +35,6 @@ __all__ = [
     "run_cli",
     "exceptions",
     "logs",
-    "config",
     "stats",
 ]
 
---
+++ airflow_dvc/custom_downloads.py
@@ -0,0 +1,53 @@
+"""
+Abstraction for DVC download targets.
+@Piotr Styczyński 2021
+"""
+from dvc_fs import DVCDownload
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
+
+class DVCS3Download(DVCDownload):
+    """
+    Download item from DVC and save it to S3
+    This is useful when you have S3Hook in your workflows used
+    as a temporary cache for files and you're not using shared-filesystem,
+    so using DVCPathDownload is not an option.
+    """
+
+    # Fields to apply Airflow templates
+    template_fields = ['dvc_path', 'bucket_name', 'bucket_path']
+
+    # Connection ID (the same as for Airflow S3Hook)
+    # For more details please see:
+    # - https://airflow.apache.org/docs/apache-airflow/1.10.14/_modules/airflow/hooks/S3_hook.html
+    # - https://www.programcreek.com/python/example/120741/airflow.hooks.S3_hook.S3Hook
+    aws_conn_id: str
+    # Bucket name (see above)
+    bucket_name: str
+    # Bucket path for the downloaded file (see above)
+    bucket_path: str
+
+    def __init__(
+        self,
+        dvc_path: str,
+        aws_conn_id: str,
+        bucket_name: str,
+        bucket_path: str,
+    ):
+        super().__init__(dvc_path=dvc_path)
+        self.aws_conn_id = aws_conn_id
+        self.bucket_name = bucket_name
+        self.bucket_path = bucket_path
+
+    def describe_target(self) -> str:
+        return f"S3 {self.bucket_name}/{self.bucket_path}"
+
+    def write(self, content: str):
+        # Open connection to the S3 and download the file
+        s3_hook = S3Hook(aws_conn_id=self.aws_conn_id)
+        s3_hook.load_string(
+            content,
+            self.bucket_path,
+            bucket_name=self.bucket_name,
+            replace=True,
+        )
