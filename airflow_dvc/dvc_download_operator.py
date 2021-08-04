3a548b0133cc8136fc73b64dcf70ce686acb89a6
Patryk Bundyra <pbundyra@covidgenomics.com>
2021-08-04 11:56:11 +0200

    Updating deprecated package

--- airflow_dvc/dvc_download_operator.py
+++ airflow_dvc/dvc_download_operator.py
@@ -5,8 +5,7 @@ Airflow operator to upload files to DVC.
 """
 from typing import Callable, List, Union
 
-from airflow.operators.python_operator import PythonOperator
-
+from airflow.operators.python import PythonOperator
 from airflow_dvc.dvc_hook import DVCHook
 from airflow_dvc.logs import LOGS
 from airflow_dvc.exceptions import add_log_exception_handler
