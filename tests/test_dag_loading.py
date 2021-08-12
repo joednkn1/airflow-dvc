06f1e38272356854cbf8a6f63ab4821830aa87ef
Piotr Styczyński <piotr@styczynski.in>
2021-08-12 16:01:25 +0200

    Change tests

--- tests/test_dag_loading.py
+++ tests/test_dag_loading.py
@@ -1,18 +1,23 @@
 import glob
 import importlib
 import os
+import pytest
 
 from airflow_dvc.tests.helpers import fake_env
 
 fake_env()
 
 
-def test_dag_loading():
-    for dag_path in glob.glob("example/dags/*.py"):
-        spec = importlib.util.spec_from_file_location(
-            os.path.basename(dag_path).split(".")[0], dag_path
-        )
-        dag_module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(dag_module)
-        assert hasattr(dag_module, "dag")
+@pytest.mark.parametrize("dag_path", glob.glob("example/dags/*.py"))
+def test_dag_loading(dag_path: str):
+    file_name = os.path.basename(dag_path).split(".")[0]
+    spec = importlib.util.spec_from_file_location(
+        file_name, dag_path
+    )
+    dag_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(dag_module)
+    assert hasattr(dag_module, "dag")
+
+    dag = dag_module.dag
+    assert dag.dag_id == file_name
 
